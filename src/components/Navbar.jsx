ec468d4d4aa2fe2cbac505b5bdbd422f8193959f
Revanth Narpala <narpala576@gmail.com>
2024-10-08 09:29:30 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src='logo192.png' />
+        <h3>ReactFacts</h3>
+        <h4>ReactCourse-Project 1</h4>
+    </div>
+  )
+}
+
+export default Navbar
